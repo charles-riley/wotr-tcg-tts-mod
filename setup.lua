7b5a38589f9fddb4a36b163a1cb0a17f14397acd
Charles Riley <charles.riley@icloud.com>
2022-12-25 09:30:58 -0500

    fellowship scenario

--- setup.lua
+++ setup.lua
@@ -27,6 +27,106 @@ shadow_helper = getObjectFromGUID("23c333")
 free_peoples_helper = getObjectFromGUID("05f876")
 turn_tracker = getObjectFromGUID("56494f")
 turn_tracker_token = getObjectFromGUID("7e74fe")
+first_player_token = getObjectFromGUID("32fc28")
+-- paths
+-- 6
+amon_hen_card_guid = "669e8b"
+-- 5
+egladil_card_guid = "7f42f3"
+dimrill_dale_card_guid = "52a02d"
+-- 3
+council_of_elrond_card_guid = "e02d69"
+-- 2
+weathertop_card_guid = "2adbc5"
+-- 1
+bag_end_card_guid = "bd3611"
+-- battlegrounds
+free_peoples_battlegrounds_deck = getObjectFromGUID("4901f5")
+lorien_card_guid = "9f9228"
+rivendell_card_guid = "b9d04e"
+shadow_battlegrounds_deck = getObjectFromGUID("a5c0cc")
+moria_card_guid = "d8e084"
+dol_guldur_card_guid = "4e536e"
+minas_morgul_card_guid = "8fbfd0"
+orthanc_card_guid = "8a4b2e"
+morannon_card_guid = "6517fe"
+-- dunedain
+blade_of_westernesse_card_guid = "8f41c3"
+boromir_card_guid = "f21a5b"
+strider_card_guid =  "85ea72"
+-- hobbit
+bilbo_card_guid = "55bc78"
+sting_card_guid = "786be7"
+there_is_another_way_card_guid = "02c180"
+mithril_coat_card_guid = "7ee22c"
+herbs_and_stewed_rabbit_card_guid = "38aabf"
+sam_gamgee_card_guid = "73ff82"
+bilbo_baggins_card_guid = "55bc78"
+fatty_bolger_card_guid = "157ad7"
+merry_brandybuck_card_guid = "55edec"
+frodo_baggins_card_guid = "600234"
+pippin_took_card_guid = "4df50b"
+-- wizard
+gandalf_the_gray_card_guid = "a0b875"
+narya_card_guid = "17c3fa"
+gandalfs_staff_card_guid = "bcc18c"
+glamdring_card_guid = "a850bc"
+treebeard_card_guid = "57b48a"
+quickbeam_card_guid = "850b55"
+gwaihir_card_guid = "5034c5"
+ent_draught_card_guid = "3f9aea"
+gandalf_the_white_card_guid = "93a4db"
+smeagol_card_guid = "8550fc"
+-- monstrous
+flocks_of_crebain_card_guid = "a44a17"
+gollum_card_guid = "af3b05"
+hill_troll_card_guid = "78a7df"
+candles_of_corpses_card_guid = "6d7cae"
+shelob_card_guid = "c0ffbe"
+balrog_of_moria_guid = "398558"
+goblins_of_the_misty_mountain_1_card_guid = "d90138"
+goblins_of_the_misty_mountain_2_card_guid = "96f44d"
+cave_troll_card_guid = "4e1fde"
+barrow_wights_card_guid = "3216df"
+caradhras_the_cruel_card_guid = "82f698"
+-- isengard
+saruman_card_guid = "3f77d0"
+palantir_of_orthanc_card_guid = "fce6c0"
+ugluk_card_guid = "fc452e"
+sarumans_staff_card_guid = "884f8b"
+-- mordor
+the_hunter_card_guid = "188841"
+the_messenger_card_guid = "a3963a"
+the_black_captain_card_guid = "ddcdc8"
+the_beguiler_card_guid = "07d0e7"
+the_destroyer_card_guid = "60555b"
+the_warrior_card_guid = "aed81b"
+the_reaver_card_guid = "b506d9"
+the_witch_king_card_guid = "e77b97"
+the_commander_card_guid = "e03abd"
+the_lidless_eye_card_guid = "49920a"
+morgul_blade_card_guid = "56c214"
+mordor_orcs_1_card_guid = "bc1b07"
+mordor_orcs_2_card_guid = "d2d0cc"
+mordor_orcs_3_card_guid = "625487"
+mordor_orcs_4_card_guid = "07111a"
+mordor_orcs_5_card_guid = "389cc6"
+black_uruks_card_guid = "00f3f2"
+the_ringwraiths_are_abroad_card_guid = "dd545c"
+the_day_without_dawn_card_guid = "76a646"
+mouth_of_sauron_card_guid = "d461c9"
+trolls_of_udun_guid = "389159"
+the_black_esterling_card_guid = "5af82c"
+grond_card_guid = "fe36db"
+black_breath_card_guid = "0c2759"
+olog_hai_card_guid = "123032"
+nazguls_mantle_card_guid = "b71359"
+gorbag_and_shagrat_card_guid = "314b5a"
+black_riders_mount_card_guid = "1ff1de"
+fell_beast_card_guid = "e648b8"
+grishnakh_card_guid = "f039e0"
+-- wizard
+
 
 -- rings
 witch_king_ring = getObjectFromGUID("3ea385")
@@ -59,6 +159,8 @@ saruman_ring_pos = Vector(-24, 0.96, -28)
 frodo_ring_pos = Vector(-24, 0.96, 28)
 aragorn_ring_pos = Vector(24, 0.96, 28)
 turn_tracker_token_pos = Vector(-52, 10, 2)
+fellowship_battlegrounds_pos = Vector(50, 1.14, 4)
+fellowship_path_pos = Vector(50, 1.14, -4)
 
 -- rotations
 turn_tracker_rot = Vector(0, 90, 180)
@@ -81,6 +183,8 @@ function setUpGame()
     if scenario == "2 Player Duel" then set_up_2_player_duel() end
 
     if scenario == "3 Player Duel" then set_up_3_player_duel() end
+
+    if scenario == "Fellowship" then set_up_fellowship() end
     
     destroyObject(self)
 end
@@ -403,4 +507,176 @@ function set_up_3_player_duel()
     -- turn tracker
     turn_tracker_token.setPosition(turn_tracker_token_pos)
     turn_tracker.setRotation(turn_tracker_rot)
+end
+
+function set_up_fellowship()
+    -- turn tracker
+    turn_tracker.setRotation(turn_tracker_rot)
+    turn_tracker_token.setPosition(Vector(-48, 1.06, 2.00))
+    first_player_token.setPosition(Vector(-46, 1.24, 6))
+    first_player_token.setRotation(Vector(0, 180, 180))
+    
+    -- balrog deck
+    local shelob = monstrous.takeObject({ guid = shelob_card_guid})
+    local coc = monstrous.takeObject({ guid = candles_of_corpses_card_guid })
+    local gollum = monstrous.takeObject({ guid = gollum_card_guid})
+    trash.putObject(shelob)
+    trash.putObject(coc)
+    trash.putObject(gollum)
+    monstrous.setPosition(brown_draw)
+    monstrous.setRotation(Vector(0,180,180))
+    if include_promo == "true" then monstrous.putObject(promo.takeObject()) else trash.putObject(promo.takeObject()) end   
+    if include_promo == "true" then monstrous.putObject(promo.takeObject()) else trash.putObject(promo.takeObject()) end   
+    local black_breath_card = mordor.takeObject({ guid = black_breath_card_guid })
+    monstrous.putObject(black_breath_card)
+    local black_riders_mount_card = mordor.takeObject({ guid = black_riders_mount_card_guid})
+    monstrous.putObject(black_riders_mount_card)
+    local grishnakh_card = mordor.takeObject({ guid = grishnakh_card_guid})
+    monstrous.putObject(grishnakh_card)
+    local mordor_orcs_1_card = mordor.takeObject({ guid = mordor_orcs_1_card_guid })
+    monstrous.putObject(mordor_orcs_1_card)
+    local mordor_orcs_2_card = mordor.takeObject({ guid = mordor_orcs_2_card_guid})
+    monstrous.putObject(mordor_orcs_2_card)
+    local mordor_orcs_3_card = mordor.takeObject({ guid = mordor_orcs_3_card_guid})
+    monstrous.putObject(mordor_orcs_3_card)
+    local morgul_blade_card = mordor.takeObject({ guid = morgul_blade_card_guid})
+    monstrous.putObject(morgul_blade_card)
+    local nazguls_mantle_card = mordor.takeObject({ guid = nazguls_mantle_card_guid})
+    monstrous.putObject(nazguls_mantle_card)
+    local the_ringwraiths_are_abroad_card = mordor.takeObject({ guid = the_ringwraiths_are_abroad_card_guid })
+    monstrous.putObject(the_ringwraiths_are_abroad_card)
+    local the_beguiler_card = mordor.takeObject({ guid = the_beguiler_card_guid })
+    monstrous.putObject(the_beguiler_card)
+    local the_destroyer_card = mordor.takeObject({ guid = the_destroyer_card_guid})
+    monstrous.putObject(the_destroyer_card)
+    local the_warrior_card = mordor.takeObject({ guid = the_warrior_card_guid})
+    monstrous.putObject(the_warrior_card)
+    local the_reaver_card = mordor.takeObject({ guid = the_reaver_card_guid})
+    monstrous.putObject(the_reaver_card)
+    local the_hunter_card = mordor.takeObject({ guid = the_hunter_card_guid })
+    monstrous.putObject(the_hunter_card)
+    local the_messenger_card = mordor.takeObject({ guid = the_messenger_card_guid })
+    monstrous.putObject(the_messenger_card)
+    local the_black_esterling_card = mordor.takeObject({ guid = the_black_esterling_card_guid })
+    monstrous.putObject(the_black_esterling_card)
+    local the_commander_card = mordor.takeObject({ guid = the_commander_card_guid })
+    monstrous.putObject(the_commander_card)
+    local the_witch_king_card = mordor.takeObject({ guid = the_witch_king_card_guid })
+    monstrous.putObject(the_witch_king_card)
+    trash.putObject(mordor)
+    trash.putObject(southron)
+    local palantir_card = isengard.takeObject({ guid = palantir_of_orthanc_card_guid})
+    monstrous.putObject(palantir_card)
+    local saruman_card = isengard.takeObject({ guid = saruman_card_guid })
+    monstrous.putObject(saruman_card)
+    local sarumans_staff_card = isengard.takeObject({ guid = sarumans_staff_card_guid})
+    monstrous.putObject(sarumans_staff_card)
+    local ugluk_card = isengard.takeObject({ guid = ugluk_card_guid})
+    monstrous.putObject(ugluk_card)
+    trash.putObject(isengard)
+    monstrous.randomize()
+
+    monstrous.deal(7, "Brown")
+
+    trash.putObject(saruman_ring)
+    trash.putObject(witch_king_ring)
+
+    -- shadow helper
+    shadow_helper.setPosition(shadow_helper_pos)
+    shadow_helper.setRotation(Vector(0,90,180))
+
+    -- shadow battlegrounds
+    -- shadow_battlegrounds.randomize()
+
+    -- gandalf
+    local smeagol = wizard.takeObject({ guid = smeagol_card_guid})
+    trash.putObject(smeagol)
+    local gandalf_white = wizard.takeObject({ guid = gandalf_the_white_card_guid })
+    trash.putObject(gandalf_white)
+    local ent_draught_card = wizard.takeObject({ guid = ent_draught_card_guid })
+    trash.putObject(ent_draught_card)
+    local gwaihir_card = wizard.takeObject({ guid = gwaihir_card_guid})
+    trash.putObject(gwaihir_card)
+    local quickbeam_card = wizard.takeObject({ guid = quickbeam_card_guid })
+    trash.putObject(quickbeam_card)
+    local treebeard = wizard.takeObject({ guid = treebeard_card_guid})
+    trash.putObject(treebeard)
+    wizard.setPosition(blue_draw)
+    wizard.setRotation(Vector(0,0,180))
+    if include_promo == "true" then wizard.putObject(promo.takeObject()) else trash.putObject(promo.takeObject()) end   
+    if include_promo == "true" then wizard.putObject(promo.takeObject()) else trash.putObject(promo.takeObject()) end   
+    local blade_card = dunedain.takeObject({ guid = blade_of_westernesse_card_guid})
+    wizard.putObject(blade_card)
+    local boromir = dunedain.takeObject({ guid = boromir_card_guid})
+    wizard.putObject(boromir)
+    local strider = dunedain.takeObject({ guid = strider_card_guid})
+    wizard.putObject(strider)
+    trash.putObject(dunedain)
+    local dwarf1 = dwarf.takeObject()
+    wizard.putObject(dwarf1)
+    local dwarf2 = dwarf.takeObject()
+    wizard.putObject(dwarf2)
+    for i = 1, 13 do
+        local elf_card = elf.takeObject()
+        wizard.putObject(elf_card)
+    end
+    trash.putObject(hobbit.takeObject({guid = there_is_another_way_card_guid}))
+    trash.putObject(hobbit.takeObject({guid = herbs_and_stewed_rabbit_card_guid}))
+    for i = 1,8 do
+        wizard.putObject(hobbit.takeObject())
+    end 
+    trash.putObject(rohan)
+
+    wizard.randomize()
+
+    wizard.deal(7, "Blue")
+
+    trash.putObject(frodo_ring)
+    trash.putObject(aragorn_ring)
+
+    -- free peoples helper
+    free_peoples_helper.setPosition(free_peoples_helper_pos)
+    free_peoples_helper.setRotation(Vector(0,270,180))    
+
+    -- battlegrounds
+    shadow_battlegrounds_deck.takeObject({ guid = morannon_card_guid, position = fellowship_battlegrounds_pos + Vector(0,1,0), smooth = true})
+    shadow_battlegrounds_deck.takeObject({ guid = orthanc_card_guid, position = fellowship_battlegrounds_pos + Vector(0,2,0), smooth = true})
+    shadow_battlegrounds_deck.takeObject({ guid = dol_guldur_card_guid, position = fellowship_battlegrounds_pos + Vector(0,3,0), smooth = true})
+    free_peoples_battlegrounds_deck.takeObject({ guid = lorien_card_guid, position = fellowship_battlegrounds_pos + Vector(0,4,0), smooth = true})
+    shadow_battlegrounds_deck.takeObject({ guid = moria_card_guid, position = fellowship_battlegrounds_pos + Vector(0,5,0), smooth = true})
+    free_peoples_battlegrounds_deck.takeObject({ guid = rivendell_card_guid, position = fellowship_battlegrounds_pos + Vector(0,6,0), smooth = true})
+    shadow_battlegrounds_deck.takeObject({ guid = minas_morgul_card_guid, position = fellowship_battlegrounds_pos + Vector(0,7,0), smooth = true})
+    trash.putObject(shadow_battlegrounds_deck)
+    trash.putObject(free_peoples_battlegrounds_deck)
+
+    -- paths
+    path6.takeObject({ guid = amon_hen_card_guid, position = fellowship_path_pos + Vector(0, 1, 0), smooth = true})
+    path5.randomize()
+    for i = 1, 3 do
+        path5.takeObject({position = fellowship_path_pos + Vector(0, 2, 0), smooth = true})
+    end
+    path4.randomize()
+    for i = 1, 3 do
+        path4.takeObject({position = fellowship_path_pos + Vector(0, 3, 0), smooth = true})
+    end
+    trash.putObject(path3.takeObject({ guid = council_of_elrond_card_guid}))
+    path3.randomize()
+    for i = 1, 2 do
+        path3.takeObject({position = fellowship_path_pos + Vector(0, 4, 0), smooth = true})
+    end
+    trash.putObject(path2.takeObject({ guid = weathertop_card_guid}))
+    path2.randomize()
+    for i = 1, 2 do
+        path2.takeObject({position = fellowship_path_pos + Vector(0, 5, 0), smooth = true})
+    end
+    path1.setPosition(Vector(-50, 1 ,0))
+    trash.putObject(path1.takeObject({ guid = bag_end_card_guid}))
+    path1.randomize()
+    for i = 1, 2 do
+        path1.takeObject({position = fellowship_path_pos + Vector(0, 6, 0), smooth = true})
+    end
+    trash.putObject(path9)
+    trash.putObject(path8)
+    trash.putObject(path7)
+    trash.putObject(path6)
 end
